74cea1b0d0856de6e58b9ae8e1c0f468b017667f
Femi Osinowo <oluwafemi.osinowo@outlook.com>
2019-11-25 00:07:21 -0500

    udpates

--- prometheus_exporter.py
+++ prometheus_exporter.py
@@ -97,11 +97,7 @@ class MetricsCollector(object):
         return []
     
     def collect(self):
-        yield GaugeMetricFamily('my_gauge', 'Help text', value=7)
-        c = CounterMetricFamily('my_counter_total', 'Help text', labels=['foo'])
-        c.add_metric(['bar'], 1.7)
-        c.add_metric(['baz'], 3.8)
-        yield c
+        yield GaugeMetricFamily('test_metrics', 'This is a blanc metric', value=7)
 
     def collect_old(self):
         '''collect metrics'''
